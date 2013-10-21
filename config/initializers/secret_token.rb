4609910d98108e1bab8a61902ffd8ba98e6d4b53
brittlewis12 <brittlewis12@gmail.com>
2013-10-21 15:00:42 -0400

    initial commit! project 1 WDI: created rails shell; updated database.yml for dev env; bundled appropriate gems.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Doodads::Application.config.secret_key_base = '3451cafcb974cf2f8d41101c8d1ef6342e77623f38cdb7aa98a63883a0a2db7880e9e3296c57dfc9bdf1f367aa9a1a80d994d6602e287ae904315e7607e29354'

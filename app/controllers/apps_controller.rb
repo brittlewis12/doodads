327c099810d387932ced9a302502b7c1da066d04
brittlewis12 <brittlewis12@gmail.com>
2013-10-21 21:37:03 -0400

    move current show method to create. should not be persisting (or attempting to persist) data thru the show method.

--- app/controllers/apps_controller.rb
+++ app/controllers/apps_controller.rb
@@ -1,5 +1,4 @@
 class AppsController < ApplicationController
-
   # show fun stuff re: apps. hot list, recently updated...
   def index; end
 
@@ -26,6 +25,39 @@ class AppsController < ApplicationController
 
   def create; end
 
-  def show; end
+  def show
+    lookup_id = params[:id]
+    response = HTTParty.get(itunes_lookup lookup_id)
+    parsed = JSON.parse(response)
+    @app = App.new
+    res = parsed["results"][0]
 
+    @itunes_id = res["trackId"]
+    @name = res["trackName"]
+    @itunes_url = res["trackViewUrl"]
+    @icon_url = res["artworkUrl512"]
+    @developer = res["artistName"]
+    @developer_url = res["sellerUrl"]
+    @price = res["price"]
+   
+    @description = res["description"]
+    @current_rating = res["averageUserRatingForCurrentVersion"]
+    @current_rating_count = res["userRatingCountForCurrentVersion"]
+    @total_rating = res["averageUserRating"]
+    @total_rating_count = res["userRatingCount"]
+    
+    @category = res["primaryGenreName"]
+    @game_center_enabled = res["isGameCenterEnabled"]
+    
+    @is_universal = res["features"].include?("iosUniversal") if res["features"]
+
+    @app.save!
+
+    @screenshot_urls = res["screenshotUrls"] # enumerate each to screenshots table
+    @ipad_screenshot_urls = res["ipadScreenshotUrls"] # enumerate each to screenshots table
+  end
+
+  def itunes_lookup itunes_id
+    "https://itunes.apple.com/lookup?id=#{itunes_id}"
+  end
 end

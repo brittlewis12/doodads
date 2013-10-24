9809ae9beace008ef5d0bb6024494bb37f2cddc7
brittlewis12 <brittlewis12@gmail.com>
2013-10-24 13:58:53 -0400

    removed not null constraint from developer url in activerecord model

--- app/models/app.rb
+++ app/models/app.rb
@@ -1,5 +1,5 @@
 class App < ActiveRecord::Base
-  validates :name, :itunes_id, :itunes_url, :icon_url, :developer, :developer_url, :price, :description, :version, :current_rating, :current_rating_count, :total_rating, :total_rating_count, :category, presence: true
+  validates :name, :itunes_id, :itunes_url, :icon_url, :developer, :price, :description, :version, :current_rating, :current_rating_count, :total_rating, :total_rating_count, :category, presence: true
   validates :game_center_enabled, :is_universal, inclusion: { in: [true, false] }
   validates :itunes_id, uniqueness: true
 

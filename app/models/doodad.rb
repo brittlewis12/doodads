6ac74b8619a848f6a81ea6c275cbf58ceb3651c1
brittlewis12 <brittlewis12@gmail.com>
2013-10-27 19:13:15 -0400

    called acts_as_votable on doodad model and acts_as_voter on user model

--- app/models/doodad.rb
+++ app/models/doodad.rb
@@ -1,4 +1,6 @@
 class Doodad < ActiveRecord::Base
   belongs_to :app
   belongs_to :user
+
+  acts_as_votable
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,11 @@ class User < ActiveRecord::Base
 
   has_many :doodads
   has_many :doodaded_apps, through: :doodads, source: :app
+
+  acts_as_voter
+  # The following optional line tracks karma (up votes) for doodads a user has submitted.
+  # Each dooda has a submitter_id column that tracks the user who submitted it.
+  # The option :weight value will be multiplied to any karma from that voteable model (defaults to 1).
+  # Karma by default is only calculated from upvotes. If you pass an array to the weight option, you can count downvotes as well (below, downvotes count for half as much karma against you):
+  # has_karma :questions, :as => :submitter, :weight => [ 1, 0.5 ]
 end
